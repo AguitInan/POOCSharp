166786a3152c153a29cc458277ace16e1e25c302
unknown <aguit.inan@gmail.com>
2024-01-07 20:34:00 +0100

    feat: add Exercice04Citerne

--- Exercice04Citerne/Classes/Citerne.cs
+++ Exercice04Citerne/Classes/Citerne.cs
@@ -6,44 +6,198 @@ using System.Threading.Tasks;
 
 namespace Exercice04Citerne.Classes
 {
-    internal class Citerne
+    internal class WaterTank
     {
-        private double _poidsAVide;
-        private double _capacite;
-        private double _eau;
-        private double _niveauRemplissage;
+        private int _fillLevel;
+        private int _maxCapacity;
+        private int _weightEmpty;
 
-        public Citerne()
+        public WaterTank()
         {
-            
+
         }
 
-        public Citerne(double poidsAVide, double capacite,double eau) : this()
+        public WaterTank(int fillLevel, int maxCapacity, int weightEmpty) : this()
         {
-            PoidsAVide = poidsAVide;
-            Capacite = capacite;
-            Eau = eau;
-            NiveauRemplissage = capacite / eau * 100;
+            FillLevel = fillLevel;
+            MaxCapacity = maxCapacity;
+            WeightEmpty = weightEmpty;
         }
 
-        public double PoidsAVide { get => _poidsAVide; set => _poidsAVide = value; }
-        public double Capacite { get => _capacite; set => _capacite = value; }
-        public double Eau { get => _eau; set => _eau = value; }
-        public double NiveauRemplissage { get => _niveauRemplissage; set => _niveauRemplissage = value; }
+        public int FillLevel
+        {
+            get => _fillLevel;
+            set
+            {
+                TotalFillLevels -= _fillLevel;
+                _fillLevel = value;
+                TotalFillLevels += _fillLevel;
+            }
+        }
+
+        public int MaxCapacity { get => _maxCapacity; set => _maxCapacity = value; }
+
+        public int WeightEmpty { get => _weightEmpty; set => _weightEmpty = value; }
+
+        //public double Eau { get => _eau; set => _eau = value; }
+
+        //public int Eau
+        //{
+        //    get => _eau;
+        //    set
+        //    {
+        //        TotalFillLevels -= _eau;
+        //        _eau = value;
+        //        //TotalSalaires += value;
+        //        TotalFillLevels += _eau;
+        //    }
+        //}
+        //public int FillLevel { get => _fillLevel; set => _fillLevel = value; }
+        public int TotalWeight { get => _weightEmpty + _fillLevel; }
+        public static int TotalFillLevels { get; private set; } = 0;
 
 
-        public void PoidsTotal()
+
+        //public int TotalWeight()
+        //{
+        //    //Console.WriteLine($"Le poids total est de {_weightEmpty + _fillLevel} kg");
+        //    //_poidsTotal = _poidsAVide + (_capacite * _niveauRemplissage);
+        //    return 500;
+        //}
+        public int Fill(int amount)
         {
-            Console.WriteLine($"Le poids total est de {_poidsAVide + (_capacite * _niveauRemplissage)} kg");
-            //_poidsTotal = _poidsAVide + (_capacite * _niveauRemplissage);
+            if (amount <= 0)
+                return 0;
+            if (_fillLevel + amount > _maxCapacity)
+            {
+                int excessWater = _fillLevel + amount - _maxCapacity;
+                FillLevel = _maxCapacity;
+                return excessWater;
+            }
+            else
+            {
+                FillLevel += amount;
+                return 0;
+            }
+
         }
-        public void Remplir(double volume)
+        public int Empty(int amount)
         {
-            _eau += volume ;
+            if (amount <= 0)
+                return 0;
+            if (_fillLevel - amount < 0)
+            {
+                int waterTaken = _fillLevel;
+                FillLevel = 0;
+                return waterTaken;
+            }
+            else
+            {
+                FillLevel -= amount;
+                return amount;
+            }
+
         }
-        public void Vider(double volume)
+
+        public void AffichageCiterne()
         {
-            _eau -= volume  ;
+            string[,] citerneAscii = {
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│            │",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+                {"┌──────O─────┐",
+                "│~~~~~~~~~~~~│",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "│            │",
+                "└────────────┘"},
+            };
+            int niveauCiterne = FillLevel == 1 ? 1 : (FillLevel * 8 / MaxCapacity);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(citerneAscii[niveauCiterne, i]);
+            }
         }
     }
 }
