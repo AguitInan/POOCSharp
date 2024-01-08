410f3be26a4505cc768f72124247c68e5be5de90
unknown <aguit.inan@gmail.com>
2024-01-08 15:10:38 +0100

    feat: update Program.cs Exercice05SalarieHeritage

--- Exercice05SalarieHeritage/Program.cs
+++ Exercice05SalarieHeritage/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Exercice05SalarieHeritage.Classes;
+
+namespace Exercice05SalarieHeritage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHM ihm = new IHM();
+            ihm.Start();
+        }
+    }
+}
