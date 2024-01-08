0b956dc3227dc455cf4ede6d486639c9a10d82e1
unknown <aguit.inan@gmail.com>
2024-01-08 10:53:01 +0100

    feat: update GenerateurDeMot.cs - Exercice03Pendu

---
+++ Exercice03Pendu/Classes/IHM.cs
@@ -0,0 +1,24 @@
+﻿using Exercice03Pendu.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice03Pendu.Classes
+{
+    class IHM
+    {
+        public static void Start()
+        {
+            char lettre;
+            bool ok = false;
+            string reponse = "";
+
+            GenerateurDeMot mot = new GenerateurDeMot();
+            Pendu pendu = new Pendu(mot);
+
+            
+        }
+    }
+}
