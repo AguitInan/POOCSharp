e5b97e882d106a4458cf103d826c49946a988781
unknown <aguit.inan@gmail.com>
2024-01-08 09:24:05 +0100

    feat: add Pendu.cs - Exercice03Pendu

---
+++ Exercice03Pendu/Classes/Pendu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice03Pendu.Classes
+{
+    class Pendu
+    {
+        private GenerateurDeMot generateur;
+        private string motATrouve;
+        private int nbEssai;
+        private string masque;
+
+
+
+    }
+}
