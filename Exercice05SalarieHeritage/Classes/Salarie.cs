67948428ea2fecbfd6803fa157bb305d62fb638b
unknown <aguit.inan@gmail.com>
2024-01-08 15:14:11 +0100

    feat: add Salarie.cs Exercice05SalarieHeritage

---
+++ Exercice05SalarieHeritage/Classes/Salarie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice05SalarieHeritage.Classes
+{
+    class Salarie
+    {
+        private string matricule;
+        private string categorie;
+        private string service;
+        private string nom;
+        private decimal salaire;
+        private static int compteur = 0;
+
+        
+
+    }
+}
