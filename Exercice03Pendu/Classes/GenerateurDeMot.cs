de098080ad1bd94d23214ebd5f1ab99d31423143
unknown <aguit.inan@gmail.com>
2024-01-08 10:21:03 +0100

    feat: add GenerateurDeMot.cs - Exercice03Pendu

---
+++ Exercice03Pendu/Classes/GenerateurDeMot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice03Pendu.Classes
+{
+    class GenerateurDeMot
+    {
+        private string[] mots = new string[] { "tekken", "streetfighter", "kingoffighters", "virtuafighter", "guiltygear", "fatalfury" };
+
+        
+    }
+}
