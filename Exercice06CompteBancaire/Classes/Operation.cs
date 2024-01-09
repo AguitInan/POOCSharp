5f0e04ae539e54a8eb40d34a792a378df89ee34c
unknown <aguit.inan@gmail.com>
2024-01-09 14:02:07 +0100

    feat: update Operation.cs Exercice06CompteBancaire

--- Exercice06CompteBancaire/Classes/Operation.cs
+++ Exercice06CompteBancaire/Classes/Operation.cs
@@ -6,6 +6,16 @@ namespace Exercice06CompteBancaire.Classes
 {
     public class Operation
     {
+        //private int numero;
+        private int id;
+        private int compteId;
+        private decimal montant;
+        private static int index = 0;
+        //public int Numero { get => numero; }
+        public decimal Montant { get => montant; }
+        public int Id { get => id; set => id = value; }
+        public int CompteId { get => compteId; set => compteId = value; }
+
         
     }
 }
