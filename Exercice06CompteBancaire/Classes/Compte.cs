5aac6b92367cacb63661168c9faab56a34cc0ade
unknown <aguit.inan@gmail.com>
2024-01-09 12:03:41 +0100

    feat: add Exercice06CompteBancaire

---
+++ Exercice06CompteBancaire/Classes/Compte.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice06CompteBancaire.Classes
+{
+    class Compte
+    {
+        private string numeroCompte;
+        private double solde;
+        private Utilisateur props;
+        private List<Operation> listeOperation;
+
+        
+    }
+}
