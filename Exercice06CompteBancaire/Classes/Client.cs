6d6e2493b1d0b52fa3cfac1401d668e6aab574bd
unknown <aguit.inan@gmail.com>
2024-01-09 13:43:48 +0100

    feat: add Client.cs Exercice06CompteBancaire

---
+++ Exercice06CompteBancaire/Classes/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice06CompteBancaire.Classes
+{
+    public class Client
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private string telephone;
+
+        
+    }
+}
