467acf459aecb4dddda7a81b8ad86c2f921623bc
unknown <aguit.inan@gmail.com>
2024-01-10 11:24:13 +0100

    feat: add Rectangle.cs Exercice07Figure

---
+++ Exercice07Figure/Classes/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice07Figure.Classes
+{
+    internal class Rectangle
+    {
+        private double _longueur;
+        private double _largeur;
+    }
+}
