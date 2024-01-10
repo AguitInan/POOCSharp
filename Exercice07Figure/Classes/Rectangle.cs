b8437b53913f4f243e1ecee473bbd6078d139722
unknown <aguit.inan@gmail.com>
2024-01-10 11:32:17 +0100

    feat: update Rectangle.cs Exercice07Figure

--- Exercice07Figure/Classes/Rectangle.cs
+++ Exercice07Figure/Classes/Rectangle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Exercice07Figure.Classes
 {
@@ -13,5 +14,11 @@ namespace Exercice07Figure.Classes
 
         public double Longueur { get => _longueur; set => _longueur = value; }
         public double Largeur { get => _largeur; set => _largeur = value; }
+
+        public override string ToString()
+        {
+            return "Longueur du rectangle: " + _longueur + "\n" +
+                "Longueur du rectangle : " + _largeur;
+        }
     }
 }
