92251f90c39c2a421487b0df062a8cb51682fddf
unknown <aguit.inan@gmail.com>
2024-01-10 11:30:33 +0100

    feat: update Carre.cs Exercice07Figure

--- Exercice07Figure/Classes/Carre.cs
+++ Exercice07Figure/Classes/Carre.cs
@@ -9,5 +9,12 @@ namespace Exercice07Figure.Classes
     internal class Carre : Figure
     {
         private double _cote;
+
+        public double Cote { get => _cote; set => _cote = value; }
+
+        public override string ToString()
+        {
+            return "Longueur de côté du carré : " + _cote;
+        }
     }
 }
