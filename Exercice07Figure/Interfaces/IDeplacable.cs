440266537c3b889d15c773c29d0747236442ae28
unknown <aguit.inan@gmail.com>
2024-01-10 14:29:54 +0100

    feat: update IDeplacable.cs Exercice06CompteBancaire

--- Exercice07Figure/Interfaces/IDeplacable.cs
+++ Exercice07Figure/Interfaces/IDeplacable.cs
@@ -8,14 +8,7 @@ namespace Exercice07Figure.Interfaces
 {
     internal interface IDeplacable
     {
+        // Les méthodes écrites dans une interface sont abstraites par défaut, on a pas besoin d'écrire le mot-clé abstract manuellement
+        public bool Deplacement(double x, double y);
     }
-}
-
-
-// Une interface permet de mettre en commun des signature de méthodes qui devront être implémentées par les classes héritant de l'interface. Il s'agit en quelque sorte d'un contrat que les classes devront signer et en respecter les clauses
-internal interface IVolant
-{
-
-    // Les méthodes écrites dans une interface sont abstraites par défaut, on a pas besoin d'écrire le mot-clé abstract manuellement
-    public bool Deplacement(double x, double y);
 }
--- Exercice07Figure/Program.cs
+++ Exercice07Figure/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using Exercice07Figure.Classes;
+
+Carre carre = new();
+carre.Cote = 2;
+//carre.Origine.PosX = 3;
+//carre.Origine.PosY = 3;
+
+Console.WriteLine(carre);
