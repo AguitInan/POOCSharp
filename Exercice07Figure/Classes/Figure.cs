133c64fe97a61de2dd03d3eecd956bf47640ebcb
unknown <aguit.inan@gmail.com>
2024-01-10 11:20:39 +0100

    feat: add IDeplacable.cs Exercice07Figure

--- Exercice07Figure/Classes/Figure.cs
+++ Exercice07Figure/Classes/Figure.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercice07Figure.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Exercice07Figure.Classes
 {
-    internal abstract class Figure
+    internal abstract class Figure : IDeplacable
     {
         private Point _origine;
 
