33eaf3813b909673ce99d60bf05a63593fa67e98
unknown <aguit.inan@gmail.com>
2024-01-10 11:16:14 +0100

    feat: add Figure.cs Exercice07Figure

--- Exercice07Figure/Classes/Figure.cs
+++ Exercice07Figure/Classes/Figure.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercice07Figure.Classes
 {
-    internal class Figure
+    internal abstract class Figure
     {
+        private Point _origine;
+
+        internal Point Origine { get => _origine; set => _origine = value; }
     }
 }
+
+
+
