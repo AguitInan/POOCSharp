7821b3633bf60df407c9519d07530b0f11188f44
unknown <aguit.inan@gmail.com>
2024-01-10 11:43:43 +0100

    feat: update Carre.cs Exercice07Figure

--- Exercice07Figure/Classes/Carre.cs
+++ Exercice07Figure/Classes/Carre.cs
@@ -9,8 +9,21 @@ namespace Exercice07Figure.Classes
     internal class Carre : Figure
     {
         private double _cote;
+        private Point _pointA;
+        private Point _pointB;
+        private Point _pointC;
+        private Point _pointD;
 
         public double Cote { get => _cote; set => _cote = value; }
+        internal Point PointA { get => _pointA; set => _pointA = value; }
+        internal Point PointB { get => _pointB; set => _pointB = value; }
+        internal Point PointC { get => _pointC; set => _pointC = value; }
+        internal Point PointD { get => _pointD; set => _pointD = value; }
+
+        public bool Deplacement(double x, double y)
+        {
+            return true;
+        }
 
         public override string ToString()
         {
