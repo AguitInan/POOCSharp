976fc55925eca91b84a5126eafff201d8b97fe35
unknown <aguit.inan@gmail.com>
2024-01-10 11:14:09 +0100

    feat: add Point.cs Exercice07Figure

---
+++ Exercice07Figure/Classes/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice07Figure.Classes
+{
+    internal class Point
+    {
+        private double _posX;
+        private double _posY;
+
+        public double PosX { get => _posX; set => _posX = value; }
+        public double PosY { get => _posY; set => _posY = value; }
+
+        public override string ToString()
+        {
+            return "Position X : " + PosX + "\n" +
+                "Position Y : " + PosY;
+        }
+    }
+}
