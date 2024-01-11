9ac35331e81a041be77045cdf6db4eeaed9edfe9
unknown <aguit.inan@gmail.com>
2024-01-11 11:17:56 +0100

    feat: update Program.cs Exercice08Pile

--- Exercice08Pile/Program.cs
+++ Exercice08Pile/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercice08Pile.Classes;
+using System;
+
+namespace Exercice08Pile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHM ihm = new IHM();
+            ihm.Start();
+        }
+    }
+}
