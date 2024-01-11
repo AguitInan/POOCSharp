e7d37f9fb6be25bd0e1b1870e196dbb33e6dbaee
unknown <aguit.inan@gmail.com>
2024-01-11 11:13:17 +0100

    feat: update Personne.cs Exercice08Pile

--- Exercice08Pile/Classes/Personne.cs
+++ Exercice08Pile/Classes/Personne.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Exercice08Pile.Classes
 {
     internal class Personne
     {
+        private string _nom;
+        private string _prenom;
+        private int _age;
+
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Prenom { get => _prenom; set => _prenom = value; }
+        public int Age { get => _age; set => _age = value; }
+        
     }
 }
