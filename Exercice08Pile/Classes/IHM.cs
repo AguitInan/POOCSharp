a44459b45acf206af51ba79021cf2d8ee4f944f6
unknown <aguit.inan@gmail.com>
2024-01-11 11:43:01 +0100

    feat: update IHM.cs Exercice08Pile

--- Exercice08Pile/Classes/IHM.cs
+++ Exercice08Pile/Classes/IHM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,63 @@ namespace Exercice08Pile.Classes
 {
     internal class IHM
     {
-        static void Main(string[] args)
+
+        //private List<Compte> listeComptes;
+        //private int numeroCompte;
+
+        private Pile<string> pileChaine = new Pile<string>(4);
+        private Pile<int> pileEntier = new Pile<int>(4);
+        private Pile<Personne> pilePersonne = new Pile<Personne>(4);
+        public IHM()
         {
-            
+            //listeComptes = new List<Compte>();
         }
+
+
+
+        public void Start()
+        {
+            string choix;
+            do
+            {
+                MenuPrincipal();
+                Console.Write("Votre choix : ");
+                choix = Console.ReadLine();
+                switch (choix)
+                {
+                    case "1":
+                        Empiler();
+                        break;
+                    case "2":
+                        Depiler();
+                        break;
+                    case "3":
+                        Get();
+                        break;
+                    case "0":
+                        //Quitter la console
+                        Environment.Exit(0);
+                        break;
+                }
+
+            } while (choix != "0");
+        }
+
+        private void MenuPrincipal()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Menu principal ===");
+            Console.WriteLine("1. Empiler");
+            Console.WriteLine("2. Dépiler");
+            Console.WriteLine("3. Récupérer à X");
+            Console.WriteLine("0. Quitter\n");
+        }
+
+
+
+
+
+
+
     }
 }
