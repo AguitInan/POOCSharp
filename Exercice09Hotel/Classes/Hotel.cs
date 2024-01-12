8a2437b120ccd08c3247951de2b908838a6f695b
unknown <aguit.inan@gmail.com>
2024-01-12 12:22:34 +0100

    feat: update Hotel.cs Exercice09Hotel

--- Exercice09Hotel/Classes/Hotel.cs
+++ Exercice09Hotel/Classes/Hotel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercice09Hotel.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -9,10 +10,12 @@ namespace Exercice09Hotel.Classes
 {
     internal class Hotel
     {
+        private string _nom;
         private List<Client> _clients;
         private List<Chambre> _chambres;
         private List<Reservation> _reservations;
 
+        public string Nom { get => _nom; set => _nom = value; }
         internal List<Client> Clients { get => _clients; set => _clients = value; }
         internal List<Chambre> Chambres { get => _chambres; set => _chambres = value; }
         internal List<Reservation> Reservations { get => _reservations; set => _reservations = value; }
@@ -23,5 +26,17 @@ namespace Exercice09Hotel.Classes
             _chambres = chambres;
             _reservations = reservations;
         }
+
+
+        public Hotel(string n)
+        {
+            Nom = n;
+
+        }
+
+
+
+
+
     }
 }
