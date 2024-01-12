156092bab753cf6f5722d1af1b91fc597c7c184c
unknown <aguit.inan@gmail.com>
2024-01-12 11:49:30 +0100

    feat: update Chambre.cs Exercice09Hotel

--- Exercice09Hotel/Classes/Chambre.cs
+++ Exercice09Hotel/Classes/Chambre.cs
@@ -8,15 +8,15 @@ namespace Exercice09Hotel.Classes
 {
     internal class Chambre
     {
-        private int numero;
-        private int nbLits;
-        private decimal tarif;
-        private StatutChambre statut;
+        private int _numero;
+        private int _nbLits;
+        private decimal _tarif;
+        private StatutChambre _statut;
 
-        public int Numero { get => numero; set => numero = value; }
-        public int NbLits { get => nbLits; set => nbLits = value; }
-        public decimal Tarif { get => tarif; set => tarif = value; }
-        public StatutChambre Statut { get => statut; set => statut = value; }
+        public int Numero { get => _numero; set => _numero = value; }
+        public int NbLits { get => _nbLits; set => _nbLits = value; }
+        public decimal Tarif { get => _tarif; set => _tarif = value; }
+        public StatutChambre Statut { get => _statut; set => _statut = value; }
     }
 
     enum StatutChambre
