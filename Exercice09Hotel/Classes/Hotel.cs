b4f21b8e4a15619d1235801d0c326300ad78c686
unknown <aguit.inan@gmail.com>
2024-01-12 11:42:59 +0100

    feat: update Hotel.cs Exercice09Hotel

--- Exercice09Hotel/Classes/Hotel.cs
+++ Exercice09Hotel/Classes/Hotel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace Exercice09Hotel.Classes
 {
     internal class Hotel
     {
+        private List<Client> _clients;
+        private List<Chambre> _chambres;
+        private List<Reservation> _reservations;
+
     }
 }
