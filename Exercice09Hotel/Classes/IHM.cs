ee77c13f43d615a99e016e4f38667978cbb37de0
unknown <aguit.inan@gmail.com>
2024-01-12 12:02:44 +0100

    feat: update IHM.cs Exercice09Hotel

--- Exercice09Hotel/Classes/IHM.cs
+++ Exercice09Hotel/Classes/IHM.cs
@@ -8,5 +8,19 @@ namespace Exercice09Hotel.Classes
 {
     internal class IHM
     {
+
+
+        private void MenuPrincipal()
+        {
+            Console.WriteLine("1. Ajouter un client");
+            Console.WriteLine("2. Afficher la liste des clients");
+            Console.WriteLine("3. Afficher les réservations d'un client");
+            Console.WriteLine("4. Ajouter une réservation");
+            Console.WriteLine("5. Annuler une réservation");
+            Console.WriteLine("6. Afficher la liste des réservations");
+            Console.WriteLine("0-- Quitter\n");
+        }
+
+
     }
 }
