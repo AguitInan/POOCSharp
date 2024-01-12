ee816fea1c16a83c9b54929ad6badc99aa988eff
unknown <aguit.inan@gmail.com>
2024-01-12 12:08:53 +0100

    feat: update IHM.cs Exercice09Hotel

--- Exercice09Hotel/Classes/IHM.cs
+++ Exercice09Hotel/Classes/IHM.cs
@@ -9,6 +9,63 @@ namespace Exercice09Hotel.Classes
     internal class IHM
     {
 
+        public void Start()
+        {
+            string choix = "";
+            do
+            {
+                bool valid = false;
+                Console.WriteLine("========== Gestion des hotels =========");
+                MenuPrincipal();
+                Console.Write("Votre choix : ");
+                choix = Console.ReadLine();
+                while (valid)
+                {
+                    if (choix == "1" && choix == "2" && choix == "3" && choix == "4" && choix == "5" && choix == "6" && choix == "0")
+                        valid = true;
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Erreur de saisie, entrez votre choix : ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+
+                switch (choix)
+                {
+                    case "1":
+                        Console.Clear();
+                        ActionAjoutClient();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        ActionAfficherListeClients();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        ActionAfficherReservationClient();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        ActionAjouterReservation();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        ActionAnnulerReservation();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        ActionAfficherListeReservations();
+                        break;
+                    case "0":
+                        break;
+                }
+
+
+
+            } while (choix != "0");
+        }
+
 
         private void MenuPrincipal()
         {
