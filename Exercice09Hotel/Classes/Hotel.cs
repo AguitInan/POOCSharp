a5087f11b27f3d4b20e8a84e47fefa37a068813b
unknown <aguit.inan@gmail.com>
2024-01-12 11:44:23 +0100

    feat: update Hotel.cs Exercice09Hotel

--- Exercice09Hotel/Classes/Hotel.cs
+++ Exercice09Hotel/Classes/Hotel.cs
@@ -16,5 +16,12 @@ namespace Exercice09Hotel.Classes
         internal List<Client> Clients { get => _clients; set => _clients = value; }
         internal List<Chambre> Chambres { get => _chambres; set => _chambres = value; }
         internal List<Reservation> Reservations { get => _reservations; set => _reservations = value; }
+
+        public Hotel(List<Client> clients, List<Chambre> chambres, List<Reservation> reservations)
+        {
+            _clients = clients;
+            _chambres = chambres;
+            _reservations = reservations;
+        }
     }
 }
