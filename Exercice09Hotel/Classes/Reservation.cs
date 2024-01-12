43494a7e2920d5fb152f6f587f344c919f0bf5f6
unknown <aguit.inan@gmail.com>
2024-01-12 11:27:02 +0100

    feat: update Reservation.cs Exercice09Hotel

--- Exercice09Hotel/Classes/Reservation.cs
+++ Exercice09Hotel/Classes/Reservation.cs
@@ -4,9 +4,22 @@ using System.Text;
 
 namespace Exercice09Hotel.Classes
 {
-    class Reservation
+    internal class Reservation
     {
-        
+        private int id;
+        private DateTime dateDebut;
+        private DateTime dateFin;
+        private StatutReservation statut;
+
+        private Client client;
+
+        public int Id { get => id; set => id = value; }
+        public DateTime DateDebut { get => dateDebut; set => dateDebut = value; }
+        public DateTime DateFin { get => dateFin; set => dateFin = value; }
+        public Client Client { get => client; set => client = value; }
+        public StatutReservation Statut { get => statut; set => statut = value; }
+
+       
     }
 
 
