1fab4e66448b08e2755d44db0dba0ec14fa77323
unknown <aguit.inan@gmail.com>
2024-01-12 09:11:02 +0100

    feat: add Exercice09Hotel

---
+++ Exercice09Hotel/Classes/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+namespace Exercice09Hotel.Classes
+{
+    internal class Client
+    {
+        private int id;
+
+        private string nom;
+
+        private string prenom;
+
+        private string telephone;
+
+        //private List<Reservation> reservations;
+
+        
+
+    }
+}
