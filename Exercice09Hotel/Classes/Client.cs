857ac2674daeba6bcb397e9de5a45b7d369c0ef3
unknown <aguit.inan@gmail.com>
2024-01-12 09:13:44 +0100

    feat: update Client.cs Exercice09Hotel

--- Exercice09Hotel/Classes/Client.cs
+++ Exercice09Hotel/Classes/Client.cs
@@ -20,7 +20,15 @@ namespace Exercice09Hotel.Classes
 
         //private List<Reservation> reservations;
 
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Telephone { get => telephone; set => telephone = value; }
+
+        public List<Reservation> Reservations { get => reservations; set => reservations = value; }
+
         
+
 
     }
 }
